547eb3fcd7ac5dfec8ef0c6bf24a15fe94d759cd
Lalo <laurencioberro@hotmail.com>
2023-11-29 19:03:20 -0300

    [A] Added Zinstaller for SceneLoader

--- Runtime/Core/Installers/SceneLoaderZinstaller.cs
+++ Runtime/Core/Installers/SceneLoaderZinstaller.cs
@@ -3,15 +3,16 @@ using ScenesLoaderSystem.Core.Domain;
 using ScenesLoaderSystem.Core.InterfaceAdapters;
 using ServiceLocatorPattern;
 using UnityEngine;
+using ZenjectExtensions.Zinstallers;
 
 namespace ScenesLoaderSystem.Core.Installers
 {
-    public class SceneLoaderInstaller : MonoInstaller
+    public class SceneLoaderZinstaller : Zinstaller
     {
         [Header("References")]
         [SerializeField] private SceneDataSO _loadingScreenSceneDataSo;
         [SerializeField] private SceneDataSO _firstOpenSceneDataSo;
-
+        
         public override void Install()
         {
             ISceneLoader sceneLoader = new SceneLoader(_loadingScreenSceneDataSo.GetSceneData(), _firstOpenSceneDataSo.GetSceneData());
